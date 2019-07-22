7c968f3bc3e51635aace199921d86c7e15127b45
Paulo Moisés <paulomoises_silva@hotmail.com>
2019-07-21 22:22:24 -0300

    App

---
+++ precache-manifest.e0693ccd21c9a8414a9e04b37e10e31b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443952df63f2524f0383565be006a3b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1052c0b6f372bd39f421",
+    "url": "/static/css/main.bd45d2c5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1052c0b6f372bd39f421",
+    "url": "/static/js/main.7e6dc8b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fbca61ce5f0321ab500bdbb168b775b0",
+    "url": "/static/media/Tajawal-Light.fbca61ce.ttf"
+  }
+]);
